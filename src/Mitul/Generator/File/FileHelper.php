8c814b4ea89d50fecf1ad326a29ffd4b5b955c2f
Sameer Panjwani <sameer@maximizer-e-services.com>
2016-01-27 18:38:03 +0530

    file separator for windows

--- src/Mitul/Generator/File/FileHelper.php
+++ src/Mitul/Generator/File/FileHelper.php
@@ -6,11 +6,21 @@ class FileHelper
 {
     public function writeFile($file, $contents)
     {
+        $file = $this->platformSlashes($file);
         file_put_contents($file, $contents);
     }
 
     public function getFileContents($file)
     {
+        $file = $this->platformSlashes($file);
         return file_get_contents($file);
     }
+
+    function platformSlashes($path) {
+        if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') {
+            $path = str_replace('/', '\\', $path);
+        }
+        return $path;
+    }
+
 }
